f8ac2152fa55b7b9d2bcf550521ec7851cbc65e9
Les Aker <me@lesaker.org>
2014-02-11 00:09:28 -0500

    fix for 1.8.7

--- lib/hss.rb
+++ lib/hss.rb
@@ -27,7 +27,7 @@ module HSS
     ## Make a new handler with a config and parser
 
     def initialize(params = {})
-      params = { config: params } if params.is_a? String
+      params = { :config => params } if params.is_a? String
       load_config(params[:config])
       @helpers = []
       load_helpers(params[:helpers])
@@ -63,7 +63,7 @@ module HSS
 
     def load_helpers(helper_path = nil)
       helper_path ||= HSS::DEFAULT_LIBRARY
-      helper_path = File.absolute_path(helper_path)
+      helper_path = File.expand_path(helper_path)
       Dir.glob(helper_path + '/*').each do |helper|
         begin
           require helper
--- lib/version.rb
+++ lib/version.rb
@@ -2,5 +2,5 @@
 # Define the version
 
 module HSS
-  VERSION = '0.2.8'
+  VERSION = '0.2.9'
 end
