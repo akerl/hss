8770e8821797ad84e3ae7c9203eeec98137735ae
Les Aker <me@lesaker.org>
2018-03-07 11:36:03 -0500

    fix cops

--- lib/hss/helpers/external.rb
+++ lib/hss/helpers/external.rb
@@ -7,16 +7,12 @@ module HSS
     private
 
     def external(source, key)
-      begin
-        config = open(File.expand_path(source)) { |f| YAML.safe_load f.read }
-      rescue Psych::SyntaxError, Errno::ENOENT
-        raise "Failed to open YAML file: #{source}"
-      end
-      begin
-        key.split('.').reduce(config) { |acc, elem| acc[elem] } || raise
-      rescue StandardError
-        raise NameError, "Key not found in YAML: #{key}"
-      end
+      config = File.open(File.expand_path(source)) { |f| YAML.safe_load f.read }
+      key.split('.').reduce(config) { |acc, elem| acc[elem] } || raise
+    rescue Psych::SyntaxError, Errno::ENOENT
+      raise "Failed to open YAML file: #{source}"
+    rescue StandardError
+      raise NameError, "Key not found in YAML: #{key}"
     end
   end
 end
